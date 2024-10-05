c5e3e81f9452dfd271a8e005aba64bb4df57fe0e
Danielfs65 <164558894+Danielfs65@users.noreply.github.com>
2024-10-05 13:34:38 -0400

    Shape.java

---
+++ Shape.java
@@ -0,0 +1,6 @@
+// Shape.java
+public abstract class Shape {
+    // Abstract methods to calculate surface area and volume
+    public abstract double surfaceArea();
+    public abstract double volume();
+}
